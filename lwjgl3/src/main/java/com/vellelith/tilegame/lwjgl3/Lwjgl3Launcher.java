6d856c51c606ccc5399c74ef812783331e4f86fb
tim21 <tim-21@windowslive.com>
2020-08-19 00:55:35 +0300

    add readme

--- lwjgl3/src/main/java/com/vellelith/tilegame/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/vellelith/tilegame/lwjgl3/Lwjgl3Launcher.java
@@ -4,21 +4,24 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.vellelith.tilegame.Game;
 
-/** Launches the desktop (LWJGL3) application. */
+/**
+ * Launches the desktop (LWJGL3) application.
+ */
 public class Lwjgl3Launcher {
-	public static void main(String[] args) {
-		createApplication();
-	}
 
-	private static Lwjgl3Application createApplication() {
-		return new Lwjgl3Application(new Game(), getDefaultConfiguration());
-	}
+    public static void main(String[] args) {
+        createApplication();
+    }
 
-	private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
-		Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
-		configuration.setTitle("TileGame");
-		configuration.setWindowedMode(640, 480);
-		configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
-		return configuration;
-	}
+    private static Lwjgl3Application createApplication() {
+        return new Lwjgl3Application(new Game(), getDefaultConfiguration());
+    }
+
+    private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
+        Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
+        configuration.setTitle("TileGame");
+        configuration.setWindowedMode(640, 480);
+        configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
+        return configuration;
+    }
 }
